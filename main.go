5cb6067579b9f6e330d08235d4d3044cb1ee39df
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2025-03-09 21:54:17 +0100

    rating changed to newest rating calculations

--- main.go
+++ main.go
@@ -4,24 +4,27 @@ import (
 "fmt"
 )
 
-func getRating(maxPos int, score int) (int,int) {
-	var level int
-	var rating int
-	var ps31 int = (maxPos*310)/1000
-	var ps50 int = (maxPos*494)/1000
-	var ps60 int = (maxPos*592)/1000
-	if (score*100)/maxPos < 50 {
-		rating = ((score-ps31)*5)+1095
-	} else if (score*100)/maxPos < 60 {
-		rating = ((score-ps50)*10)+2500
-	} else {
-		rating = ((score-ps60)*20)+4050
+func getRating(maxPos int, score int) (int, int) {
+    var level int
+    var rating int
+    var ps30 int = (maxPos * 300) / 1000
+    var ps50 int = (maxPos * 500) / 1000
+    var ps60 int = (maxPos * 600) / 1000
+	if (score * 100) / maxPos < 30 {
+		return 1, 0
 	}
-	if rating < 500 {
-		return 1, rating
-	}
-	level = ((rating-500)/75)+2
-	return level, rating
+    if (score * 100) / maxPos < 50 {
+        rating = ((score - ps30) * 7)
+    } else if (score * 100) / maxPos < 60 {
+        rating = (((score - ps50) * 95))/10 + 2520
+    } else {
+        rating = ((score - ps60) * 20) + 4230
+    }
+    if rating < 500 {
+        return 1, rating
+    }
+    level = ((rating - 500) / 75) + 2
+    return level, rating
 }
 
 func main() {
