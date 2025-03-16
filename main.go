5dd1cc9991368507f7f35c24c6f4f03344fae26d
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2025-03-16 04:50:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,26 +5,37 @@ import (
 )
 
 func getRating(maxPos int, score int) (int, int) {
-    var level int
-    var rating int
-    var ps30 int = (maxPos * 300) / 1000
-    var ps50 int = (maxPos * 500) / 1000
-    var ps60 int = (maxPos * 600) / 1000
-	if (score * 100) / maxPos < 30 {
-		return 1, 0
+	var level, rating, perM int
+	var PRM = []int {0, 25, 925, 1275, 2500, 3950, 4700, 5300, 5750, 6275}
+	perM = (score * 1000) / maxPos	
+	if perM < 250 {
+		rating = ((PRM[0] / (25-0)) * perM) / 10 // 2 rating per promile onder 250 promile
+	} else if perM < 300 {
+		rating = PRM[0] + ((((PRM[1]-PRM[0]) / (30-25)) * (perM-250)) / 10) //X rating per promile boven 250 promile 
+	} else if perM < 400 {
+		rating = PRM[1] + ((((PRM[2]-PRM[1]) / (40-30)) * (perM-300)) / 10) //X rating per promile boven 300 promile
+	} else if perM < 450 {
+		rating = PRM[2] + ((((PRM[3]-PRM[2]) / (45-40)) * (perM-400)) / 10) //X rating per promile boven 300 promile
+	} else if perM < 500 {
+		rating = PRM[3] + ((((PRM[4]-PRM[3]) / (50-45)) * (perM-450)) / 10) //X rating per promile boven 400 promile
+	} else if perM < 530 {
+		rating = PRM[4] + ((((PRM[5]-PRM[4]) / (53-50)) * (perM-500)) / 10) //X rating per promile boven 500 promile
+	} else if perM < 550 {
+		rating = PRM[5] + ((((PRM[6]-PRM[5]) / (55-53)) * (perM-530)) / 10) //X rating per promile boven 530 promile
+	} else if perM < 570 {
+		rating = PRM[6] + ((((PRM[7]-PRM[6]) / (57-55)) * (perM-550)) / 10) //X rating per promile boven 550 promile
+	} else if perM < 600 {
+		rating = PRM[7] + ((((PRM[8]-PRM[7]) / (60-57)) * (perM-570)) / 10) //X rating per promile boven 570 promile
+	} else if perM < 650 {
+		rating = PRM[8] + ((((PRM[9]-PRM[8]) / (65-60)) * (perM-600)) / 10) //12 rating per promile ONDER 650 PROMILE
+	} else {
+		rating = PRM[9] + ((perM-700)*20) //20 rating per promile boven 700 promile
 	}
-    if (score * 100) / maxPos < 50 {
-        rating = ((score - ps30) * 7)
-    } else if (score * 100) / maxPos < 60 {
-        rating = (((score - ps50) * 95))/10 + 2520
-    } else {
-        rating = ((score - ps60) * 20) + 4230
-    }
-    if rating < 500 {
-        return 1, rating
-    }
-    level = ((rating - 500) / 75) + 2
-    return level, rating
+	if rating < 500 {
+		return 1, rating
+	}
+	level = ((rating - 500) / 75) + 2
+	return level, rating
 }
 
 func main() {
